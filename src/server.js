1842a3324c8f5d98fe84641ea55e239962b5f2b6
Mateusz <matzaj101@gmail.com>
2022-04-06 22:55:01 +0200

    basic server

---
+++ src/server.js
@@ -0,0 +1,10 @@
+﻿const express = require("express");
+const app = express();
+
+app.use("/", (req, res, next) => {
+  res.send("ok");
+});
+
+app.listen(9999, () => {
+  console.log("appi up and running ");
+});
